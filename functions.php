a68d22c207ba82b6b61e633eece9ddb97ea0c183
Rachel Fryan <rachelafryan@gmail.com>
2016-11-07 18:24:55 -0600

    js file

--- functions.php
+++ functions.php
@@ -117,7 +117,7 @@ function blank_scripts() {
 
 	wp_enqueue_script( 'blank-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'blank-base', get_template_directory_uri() . '/js/base.js', array(), '20151215', true );
+	wp_enqueue_script( 'blank-base', get_template_directory_uri() . '/js/base.js', array ( 'jquery' ), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
